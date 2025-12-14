57f8f7f34bd297b55c93b2b539e39d2d6d88cacc
zulfqara <zulfiqaraale@gmail.com>
2025-12-14 21:50:16 +0500

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,9 +1,16 @@
+--Master is a system database which is used to create new databases
 USE master
 Go
+
+  --Here we created the new database
 create database datawareh ouse;
 Go
+
+  --Switched the database from master to datawarehouse
 use datawarehouse;
 Go
+
+  --Creating schemas for each layer
 Create Schema bronze;
 Go
 Create Schema silver;
